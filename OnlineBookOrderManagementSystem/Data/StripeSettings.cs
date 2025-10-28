937bca2901b63fbd1582fdddc377c718dd555530
Mahin05 <mahin.islam05@gmail.com>
2025-10-28 22:48:12 +0600

    stripe setting class added

---
+++ OnlineBookOrderManagementSystem/Data/StripeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineBookOrderManagementSystem.Data
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; }
+        public string PublishabelKey { get; set; }
+
+    }
+}
